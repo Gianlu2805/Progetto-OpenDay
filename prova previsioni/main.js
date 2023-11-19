73432ce91246fed4b938909d12035fe3cc2e554a
Gianluca <gianluca.galanti77@gmail.com>
2023-11-19 12:03:32 +0100

    changed console.log

--- prova previsioni/main.js
+++ prova previsioni/main.js
@@ -1,5 +1,5 @@
 //apikey = EVPUPWJLED7AAULMJBMDVB3GJ
-dataUrl = new URL("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=Urbino,PU,61034&aggregateHours=24&unitGroup=us&shortColumnNames=false&contentType=json&key=EVPUPWJLED7AAULMJBMDVB3GJ");
+dataUrl = new URL("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=Urbino,PU,61034&aggregateHours=24&unitGroup=metric&shortColumnNames=false&contentType=json&key=EVPUPWJLED7AAULMJBMDVB3GJ");
 var i; 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -25,5 +25,5 @@ function getPrevision()
 function getForecastData(response)
 {
     console.log(response);
-    console.log(response.locations["Urbino,PU,61034"].currentConditions.temp);
+    console.log(response.locations["Urbino,PU,61034"].values[15].conditions);
 }
